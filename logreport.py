0c12fbee280685ca0e671a2e0228d6ac9b7f271d
Revanth Reddy Gopavarapu <revanthkng@gmail.com>
2018-08-04 12:28:50 -0700

    HTTP requests, response

---
+++ logreport.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+
+        self.send_header('content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        self.wfile.write("Hello, My First Project! \n Hope you will be successful".encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, HelloHandler)
+    httpd.serve_forever()
